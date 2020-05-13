b4ea86ff103a49c27e787089f84fc8ad548ea1ac
adang <git@arvindang.com>
2020-05-13 10:08:20 -0400

    Generating unique game page url on creation of new game. Displaying host name on new game show page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'games#index'
-  resources :games
+  # resources :games
+
+  get '/games', to: 'games#index'
+  get 'games/new', to: 'games#new'
+  get 'games/:id', to: 'games#show'
+  post 'games', to: 'games#create'
+  delete 'games/:id', to: 'games#destroy'
+
+
+  # get '/patients/:id', to: 'patients#show'
+  # get 'books/:id' to: 'books/show'
+
+  # GET "/games/:bigolurl", to: "games#show"
 
 end
